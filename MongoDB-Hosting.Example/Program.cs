cc56fe80ca0902361a4879b4376edd3567cf8f82
kampfmodz <43520105+kampfmodz@users.noreply.github.com>
2022-06-26 21:25:50 +0200

    Update Program.cs

--- MongoDB-Hosting.Example/Program.cs
+++ MongoDB-Hosting.Example/Program.cs
@@ -5,7 +5,7 @@ using MongoDB_Hosting.Example;
 var builder = Host.CreateDefaultBuilder();
 builder.ConfigureServices(services =>
 {
-    services.AddMongoService("mongodb://kampfmodz:lukas1707@localhost:27017/test?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false");
+    services.AddMongoService("YourMongoUri");
     services.AddSingleton<Random>();
 
     services.AddHostedService<ExampleService>();
